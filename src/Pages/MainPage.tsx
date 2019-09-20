1511d79bf4e4092b1c64c958d349ac4ef42d59d0
Erick Gomez <erick0gomez98@gmail.com>
2019-09-19 21:40:15 -0500

    se hacen pruebas

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const MainPage = () => {
+    const [count, setCount] = useState<number>(0);
+    const videoRef = useRef<HTMLVideoElement>(null);
+    console.log(videoRef);
+
+
+    useEffect(() => {
+        console.log("component did mount!")
+        console.log(videoRef.current);
+        if (videoRef.current) {
+            console.log(videoRef.current.id)
+        }
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then((stream) => {
+                //videoRef.srcObject = stream;
+                console.log(stream)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, []);
     return (
         <>
-            Main Page
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setCount(count - 1)}>-</button>
+
+
+            <video id="player" ref={videoRef} controls></video>
         </>
     )
 }
